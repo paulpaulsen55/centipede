162759613b4618b3637423f0446410ed8034cbb1
Justus <justus.winter@urbanandmainlines.com>
2024-02-15 15:02:27 +0100

    added triangle test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ int main()
 {
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
+
+    // font test
     sf::Text text;
     sf::Font font;
     font.loadFromFile("assets/arial.ttf");
@@ -11,7 +13,14 @@ int main()
     text.setString("Hello, World!");
     text.setCharacterSize(100);
 
-
+    // triangle
+    sf::VertexArray triangle(sf::Triangles, 3);
+    triangle[0].position = sf::Vector2f(110.f, 310);
+    triangle[1].position = sf::Vector2f(600.f, 310);
+    triangle[2].position = sf::Vector2f(200.f, 600);
+    triangle[0].color = sf::Color::Red;
+    triangle[1].color = sf::Color::Green;
+    triangle[2].color = sf::Color::Blue;
 
     while (window.isOpen())
     {
@@ -25,6 +34,7 @@ int main()
 
         window.clear();
         window.draw(text);
+        window.draw(triangle);
         window.display();
     }
 }
