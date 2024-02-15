b845565b8ea7ec68e22e37ed6dfb81883a7e2761
Justus <justus.winter@urbanandmainlines.com>
2024-02-15 15:16:17 +0100

    added namespace sf

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,34 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
 
     // font test
-    sf::Text text;
-    sf::Font font;
+    Text text;
+    Font font;
     font.loadFromFile("assets/arial.ttf");
     text.setFont(font);
     text.setString("Hello, World!");
     text.setCharacterSize(100);
 
     // triangle
-    sf::VertexArray triangle(sf::Triangles, 3);
-    triangle[0].position = sf::Vector2f(110.f, 310);
-    triangle[1].position = sf::Vector2f(600.f, 310);
-    triangle[2].position = sf::Vector2f(200.f, 600);
-    triangle[0].color = sf::Color::Red;
-    triangle[1].color = sf::Color::Green;
-    triangle[2].color = sf::Color::Blue;
+    VertexArray triangle(Triangles, 3);
+    triangle[0].position = Vector2f(110.f, 310);
+    triangle[1].position = Vector2f(600.f, 310);
+    triangle[2].position = Vector2f(200.f, 600);
+    triangle[0].color = Color::Red;
+    triangle[1].color = Color::Green;
+    triangle[2].color = Color::Blue;
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        for (auto event = Event{}; window.pollEvent(event);)
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
             {
                 window.close();
             }
